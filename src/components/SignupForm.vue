ff2b31789723041316aaa0bcbc4a423efb7f93a8
annamaria <szumiecam@gmail.com>
2024-02-03 16:54:24 +0100

    added Signup&Login forms

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="name" v-model="name" />
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <br />
+    <br />
+    <button>Sign up</button>
+  </form>
+</template>
+  
+  <script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    // refs
+    const name = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = () => {
+      console.log(name.value, email.value, password.value);
+    };
+
+    return { name, email, password, handleSubmit };
+  },
+};
+</script>
+  
+  <style>
+</style>
