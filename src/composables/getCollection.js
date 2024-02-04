d61e10eeb3b7b9fd1f9a9aebd21525b19429f674
annamaria <szumiecam@gmail.com>
2024-02-04 17:54:34 +0100

    added real-time firestore listener

---
+++ src/composables/getCollection.js
@@ -0,0 +1,26 @@
+import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+const getCollection = (collection) => {
+    const documents = ref(null)
+    const error = ref(null)
+
+    let collectionRef = projectFirestore.collection(collection)
+        .orderBy('createdAt')
+
+    collectionRef.onSnapshot(() => {
+        let results = []
+        snap.docsforEach(doc => {
+            doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
+        })
+        documents.value = results
+        error.value = null
+    }, (err) => {
+        console.log(err.message)
+        documents.value = null
+        error.value = 'could not fetch data'
+    })
+    return { documents, error }
+}
+
+export default getCollection
